f5ca5794d46935459d7366f6fdf021e4b0fefed5
user.email <sawepeter6@gmail.com>
2018-07-13 03:57:23 +0300

    migrations successfully made

--- journey/baords/models.py
+++ journey/baords/models.py
@@ -9,16 +9,16 @@ class Board(models.Model):
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now_add=True)
-   # board = models.ForeignKey(Board, related_name='topics')
-   # starter = models.ForeignKey(User, related_name='topics')
+    board = models.ForeignKey(Board, related_name='topics', on_delete=models.CASCADE)
+    starter = models.ForeignKey(User, related_name='topics', on_delete=models.CASCADE)
 
 class Post(models.Model):
     message = models.TextField(max_length=4000)
-   # topic = models.ForeignKey(Topic, related_name='posts')
+    topic = models.ForeignKey(Topic, related_name='posts', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
-   # created_by = models.ForeignKey(User, related_name='posts')
-   # updated_by = models.ForeignKey(User, null=True, related_name='+')
+    created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE, default='Sawe')
+    updated_by = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name='+')
 
 
 
