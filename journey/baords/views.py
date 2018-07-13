e62bca4c2a02283abe174fb44e3d16c41fdd6f9c
user.email <sawepeter6@gmail.com>
2018-07-13 05:07:27 +0300

    homepage testing

--- journey/baords/views.py
+++ journey/baords/views.py
@@ -7,11 +7,4 @@ from .models import Board
 
 def home(request):
     baords = Board.objects.all()
-    baords_names = list()
-
-    for board in baords:
-        baords_names.append(board.name)
-
-    response_html = '<br>'.join(baords_names)
-
-    return HttpResponse(response_html)
+    return render(request, 'home.html', {'baords':baords})
