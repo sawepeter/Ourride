bfb597c78e77f1d360f9ccd5d697a31d69efc2cd
user.email <sawepeter6@gmail.com>
2018-07-13 04:46:51 +0300

    shell tests

--- journey/baords/views.py
+++ journey/baords/views.py
@@ -2,5 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Board
+
+
 def home(request):
-    return HttpResponse('Hello, world!')
+    baords = Board.objects.all()
+    baords_names = list()
+
+    for board in baords:
+        baords_names.append(board.name)
+
+    response_html = '<br>'.join(baords_names)
+
+    return HttpResponse(response_html)
