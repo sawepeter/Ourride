72bb0b8eb6edb59a224a96acbb94da431a757ec5
user.email <sawepeter6@gmail.com>
2018-07-13 05:35:11 +0300

    static files setup

--- journey/baords/tests.py
+++ journey/baords/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.urls import resolve, reverse
+
+from .views import home
+
+
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEquals(view.func, home)
