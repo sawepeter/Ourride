4eb632016572320d5c7418ff3df87bd0a5f22f5e
sawepeter <sawepeter6@gmail.com>
2018-09-19 08:25:30 +0300

    tests 🚧🔥

--- journey/baords/admin.py
+++ journey/baords/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Board
+from .models import Topic
+from .models import Post
+
+admin.site.register(Board)
+admin.site.register(Topic)
+admin.site.register(Post)
+
